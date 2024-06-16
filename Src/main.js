f8091e1d78e4fb42ea5f561109024668736fc257
unknown <kalungi.roberts2000@gmail.com>
2024-06-16 17:27:54 +0200

    first commit

---
+++ Src/main.js
@@ -0,0 +1,32 @@
+const imageContainerEL = document.querySelector(".image-container");
+
+const prevEL = document.getElementById("prev");
+const nextEL = document.getElementById("next");
+let x = 0;
+let timer;
+prevEL.addEventListener("click", ()=> {
+    x = x + 45;
+    clearTimeout(timer);
+    updateGallery();
+});
+
+nextEL.addEventListener("click", ()=> {
+    x = x - 45;
+    clearTimeout(timer);
+    updateGallery();
+});
+
+
+
+function updateGallery() { 
+    imageContainerEL.style.transform = 
+    `perspective(1000px) rotateY(${x}deg)`;
+    timer = setTimeout(()=>{
+        x = x -45;
+        clearTimeout();
+        updateGallery();
+    }, 3000);
+ }
+ updateGallery();
+ console.log(updateGallery);
+ 
